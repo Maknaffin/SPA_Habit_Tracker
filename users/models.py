c1cbc60335785262f41c1633b2ef967ad67883b5
Timofey <tima.khlystov99@mail.ru>
2024-12-27 03:38:52 +0300

    Initial commit

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email')
+    name = models.CharField(max_length=50, verbose_name='имя пользователя', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
